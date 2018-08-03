270ed1e18d686bc1afaa6299baee026b18a9c451
abansal <abansal@local>
2018-08-03 18:55:22 +0000

    my lab1 work

--- lab1/text_recognizer/models/character_model.py
+++ lab1/text_recognizer/models/character_model.py
@@ -21,7 +21,11 @@ class CharacterModel(Model):
             image = (image / 255).astype(np.float32)
         # NOTE: integer to character mapping dictionary is self.data.mapping[integer]
         ##### Your code below (Lab 1)
-
+        # use the model to predict a class label
+        pred_raw = self.network.predict(np.expand_dims(image, 0), batch_size=1).flatten()
+        ind = np.argmax(pred_raw)
+        confidence_of_prediction = pred_raw[ind]
+        predicted_character = self.data.mapping[ind]
         ##### Your code above (Lab 1)
         return predicted_character, confidence_of_prediction
 
